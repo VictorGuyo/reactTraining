55b664d254180bd95fb8bc15123cf5426440c8aa
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-11-16 21:36:52 +0100

    second test

--- notreProjet/src/App.jsx
+++ notreProjet/src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
+      <h1>Test</h1>
+      <h2>Ceci est un test</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
