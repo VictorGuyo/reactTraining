ade8af98ac264e84a0225d7895fdbe21f3652a1a
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-11-17 01:58:39 +0100

    ptit css

--- notreProjet/src/components/Filters.jsx
+++ notreProjet/src/components/Filters.jsx
@@ -12,10 +12,10 @@ const {setCategory, category} = useApi();
     }
     return (
         <div>
-            <button value="men's clothing" onClick={handleClick}>Vêtements Hommes</button>
-            <button value="jewelry">Bijoux</button>
-            <button value="electronics">Électroniques</button>
-            <button value="women's clothing">Vêtements Femmes</button>
+            <button className='button' value="men's clothing" onClick={handleClick}>Vêtements Hommes</button>
+            <button className='button' value="jewelry">Bijoux</button>
+            <button className='button' value="electronics">Électroniques</button>
+            <button className='button' value="women's clothing">Vêtements Femmes</button>
         </div>
     );
 };
