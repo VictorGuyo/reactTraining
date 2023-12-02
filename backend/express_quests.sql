ad187124c38cb4053773a2f07012fd764160e63e
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-12-02 23:21:10 +0100

    backend + SQL

--- backend/express_quests.sql
+++ backend/express_quests.sql
@@ -1,113 +1,61 @@
-DROP TABLE IF EXISTS movies;
+-- SQLBook: Code
+-- Active: 1701552274622@@127.0.0.1@3306@express_quests
 
-CREATE TABLE movies (
-  id int primary key NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL,
-  director varchar(255) NOT NULL,
-  year varchar(255) NOT NULL,
-  color varchar(255) NOT NULL,
-  duration int NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3;
-
-INSERT INTO
-  movies (title, director, year, color, duration)
-VALUES
-  (
-    'Citizen Kane',
-    'Orson Wells',
-    '1941',
-    '0',
-    120
-  ),
-  (
-    'The Godfather',
-    'Francis Ford Coppola',
-    '1972',
-    '1',
-    180
-  ),
-  (
-    'Pulp Fiction',
-    'Quentin Tarantino',
-    '1994',
-    '1',
-    180
-  ),
-  (
-    'Apocalypse Now',
-    'Francis Ford Coppola',
-    '1979',
-    '1',
-    150
-  ),
-  (
-    '2001 a space odyssey',
-    'Stanley Kubrick',
-    '1968',
-    '1',
-    160
-  ),
-  (
-    'The Dark Knight',
-    'Christopher Nolan',
-    '2008',
-    '1',
-    150
-  );
-
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    id int primary key NOT NULL AUTO_INCREMENT,
-    firstname varchar(255) NOT NULL,
-    lastname varchar(255) NOT NULL,
-    email varchar(255) UNIQUE NOT NULL,
-    city varchar(255) DEFAULT NULL,
-    language varchar(255) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE
+    items (
+        id int primary key NOT NULL AUTO_INCREMENT,
+        title varchar(255) NOT NULL,
+        price INT NOT NULL,
+        description varchar(350) NOT NULL,
+        category varchar(255) NOT NULL,
+        image varchar(255) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3;
 
 INSERT INTO
-  users (firstname, lastname, email, city, language)
+    items (
+        title,
+        price,
+        description,
+        category,
+        image
+    )
 VALUES
-  (
-    'John',
-    'Doe',
-    'john.doe@example.com',
-    'Paris',
-    'English'
-  ),
-  (
-    'Valeriy',
-    'Appius',
-    'valeriy.appius@example.com',
-    'Moscow',
-    'Russian'
-  ),
-  (
-    'Ralf',
-    'Geronimo',
-    'ralf.geronimo@example.com',
-    'New York',
-    'Italian'
-  ),
-  (
-    'Maria',
-    'Iskandar',
-    'maria.iskandar@example.com',
-    'New York',
-    'German'
-  ),
-  (
-    'Jane',
-    'Doe',
-    'jane.doe@example.com',
-    'London',
-    'English'
-  ),
-  (
-    'Johanna',
-    'Martino',
-    'johanna.martino@example.com',
-    'Milan',
-    'Spanish'
-  );
+        (
+        "Mens Casual Premium Slim Fit T-Shirts ",
+        22.3,
+        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric. ",
+        "men's clothing",
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+    ), (
+        "Mens Cotton Jacket",
+        55.99,
+        "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. ",
+        "men s clothing",
+        "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+    ), (
+        "Mens Casual Slim Fit",
+        15.99,
+        "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+        "men's clothing",
+        "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
+    ), (
+        "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+        695,
+        "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+        "jewelery",
+        "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+    ), (
+        "Solid Gold Petite Micropave ",
+        168,
+        "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+        "jewelery",
+        "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
+    ), (
+        "White Gold Plated Princess",
+        9.99,
+        "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+        "jewelery",
+        "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg"
+    );
+
+
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT
 
-const app = express();
-
-app.use(express.json());
-
-const movieControllers = require("./controllers/movieControllers");
-
-app.get("/api/movies", movieControllers.getMovies);
-app.get("/api/movies/:id", movieControllers.getMovieById);
-
-app.get("/api/users", movieControllers.getUser);
-
-app.get("/api/users/:id", movieControllers.getUserById);
-
-app.post("/api/movies", movieControllers.postMovie);
-app.post("/api/users", movieControllers.postUser);
 
 module.exports = app;
